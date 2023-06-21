203d83e2e9d17c691bea46c3aec02275403541d9
alexdavid723 <alexdavidxela@gmail.com>
2023-06-21 18:35:47 -0500

    microservicios

---
+++ service-product/src/main/java/com/quispcs/serviceproduct/entity/Producto.java
@@ -0,0 +1,34 @@
+package com.quispcs.serviceproduct.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name="tabla_producto")//para definir el nombre de la tabla
+@Data //para generar los metodos getter y setter
+public class Producto {
+    //Para mapear nuestra entidad
+    // e identificar nuestro Primary Key y para que se autoincremente con el @generatedvalue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private Double stock;
+    private Double precio;
+    private String estado;
+    @Column(name ="create_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createAt;
+    //para mapear usamos many to one  osea una categoria tiene muchos productos
+    @ManyToOne(fetch = FetchType.LAZY)
+    //usamos fetchtype.lazy paracargar valore en un momento que es requerido
+
+    @JoinColumn(name="category_id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+    private Category category;
+}
+//luego crear repository para manipular datos
