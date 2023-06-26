6d81cd957bbea85fe7c27e393dfa297a5d89ccac
alexdavid723 <alexdavidxela@gmail.com>
2023-06-25 21:48:43 -0500

    realizando correcciones

--- Auth/src/main/java/com/quispcs/auth/security/JwtProvider.java
+++ Auth/src/main/java/com/quispcs/auth/security/JwtProvider.java
@@ -3,7 +3,7 @@ package com.quispcs.auth.security;
 import com.quispcs.auth.entity.AuthUser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import jakarta.annotation.PostConstruct;
+import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
