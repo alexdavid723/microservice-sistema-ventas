8266217e86e3cadf48adba9a704ee783b270e7f0
alexdavid723 <alexdavidxela@gmail.com>
2023-06-25 22:45:45 -0500

    integrando el adm

--- service-venta/src/main/java/com/quispcs/serviceventa/ServiceVentaApplication.java
+++ service-venta/src/main/java/com/quispcs/serviceventa/ServiceVentaApplication.java
@@ -1,9 +1,12 @@
 package com.quispcs.serviceventa;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -13,5 +16,14 @@ public class ServiceVentaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceVentaApplication.class, args);
 	}
-
+	@Bean
+	public OpenAPI custoOpenAPI() {
+		return new OpenAPI().info(new Info()
+				.title("OPEN API MICROSERVICIOS VENTA")
+				.version("0.0.1")
+				.description("servicio web venta")
+				.termsOfService("http://swagger.io/terms")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org"))
+		);
+	}
 }
